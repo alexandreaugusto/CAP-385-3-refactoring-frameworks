1242f7598a2f9b8f7c9921ddfe86f64cd4c44f68
Alexandre A. D. de Oliveira <correiodoalexandre@gmail.com>
2016-07-24 01:04:33 -0300

    Adding files to repository

---
+++ src/org/cbsoft/framework/Crypto.java
@@ -0,0 +1,19 @@
+package org.cbsoft.framework;
+
+public class Crypto implements PostProcessor {
+	
+	private int delay;
+	
+	public Crypto(int delay) {
+		this.delay = delay;
+	}
+
+	public byte[] postProcess(byte[] bytes) {
+		byte[] newBytes = new byte[bytes.length];
+		for(int i=0;i<bytes.length;i++){
+			newBytes[i]= (byte) ((bytes[i]+delay) % Byte.MAX_VALUE);
+		}
+		return newBytes;
+	}
+
+}
